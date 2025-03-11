82cb833dcb0d4b806d30c492f7ee3aa2058fc38f
NitroRonak <ronaks0988@gmail.com>
2025-03-12 00:45:40 +0530

    User Model Done

---
+++ Server/models/UserModel.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  firstName: {
+    type: String,
+    required: false,
+  },
+  lastName: {
+    type: String,
+    required: false,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  color: {
+    type: Number,
+    required: false,
+  },
+  profileSetup: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default mongoose.model("User", userSchema);
