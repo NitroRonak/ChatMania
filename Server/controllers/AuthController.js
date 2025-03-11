61cd71178d6f9ca599a2c30d5b98106acc8bbddf
NitroRonak <ronaks0988@gmail.com>
2025-03-12 02:01:09 +0530

    Login controller done

--- Server/controllers/AuthController.js
+++ Server/controllers/AuthController.js
@@ -14,7 +14,7 @@ export const signup = async (req, res) => {
         if(user){
             return res.status(400).send("User already exists");
         }
-        const newUser = User.create({email, password});
+        const newUser = await User.create({email, password});
         const token = createToken(email,newUser._id);
         res.cookie("jwt",token,{
             maxAge:maxAge,
@@ -32,4 +32,41 @@ export const signup = async (req, res) => {
      console.log(error);
      return res.status(500).send("Internal Server Error");
     }
-} 
+}
+
+export const login = async (req, res) => {
+    try {
+       const {email,password} = req.body;
+       if(!email || !password){
+        return res.status(400).send("Email and password are required");
+       }
+       const user = await User.findOne({email});
+       if(!user){
+        return res.status(400).send("User does not exist");
+       }
+       const match = await bcrypt.compare(password,user.password);
+       if(!match){
+        return res.status(400).send("Invalid credentials");  
+       }
+       const token = createToken(email,user._id);
+       res.cookie("jwt",token,{
+        maxAge:maxAge,
+        secure:true,
+        sameSite:"None",
+       });
+       return res.status(200).json({
+        user:{
+            email,
+            id:user._id,
+            profileSetup:user.profileSetup,
+            firstName:user.firstName,
+            lastName:user.lastName,
+            image:user.image,
+            color:user.color
+        }
+       })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("Internal Server Error");
+    }
+}
