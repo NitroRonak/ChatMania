8a25f0345db60cdeeae53d4886b60ea37fdfc84d
NitroRonak <ronaks0988@gmail.com>
2025-03-12 02:03:36 +0530

    login constant added

--- Client/src/utils/constants.ts
+++ Client/src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const HOST: string = import.meta.env.VITE_SERVER_URL
 export const AUTH_ROUTES: string = `/api/auth`
 export const SIGNUP_ROUTE: string = `${AUTH_ROUTES}/signup`
+export const SIGNIN_ROUTE: string = `${AUTH_ROUTES}/login`
