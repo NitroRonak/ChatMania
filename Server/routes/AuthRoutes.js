f8ec2f3968bbde42d488689aaaccdc3a965478b8
NitroRonak <ronaks0988@gmail.com>
2025-03-12 17:46:36 +0530

    userinfo middleware and route done.

--- Server/routes/AuthRoutes.js
+++ Server/routes/AuthRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup,login } from "../controllers/AuthController.js";
+import { signup,login,getUserInfo } from "../controllers/AuthController.js";
+import { verifyToken } from "../middlewares/AuthMiddleware.js";
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-
+router.get("/user-info",verifyToken, getUserInfo)
 export default router;
