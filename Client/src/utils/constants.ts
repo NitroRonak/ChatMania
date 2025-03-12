687e239834641185d3856a20d737d495add86bed
NitroRonak <ronaks0988@gmail.com>
2025-03-13 01:00:10 +0530

    profile route added

--- Client/src/utils/constants.ts
+++ Client/src/utils/constants.ts
@@ -3,3 +3,5 @@ export const AUTH_ROUTES: string = `/api/auth`
 export const SIGNUP_ROUTE: string = `${AUTH_ROUTES}/signup`
 export const SIGNIN_ROUTE: string = `${AUTH_ROUTES}/login`
 export const GET_USER_INFO:string = `${AUTH_ROUTES}/user-info`
+
+export const UPDATE_PROFILE_ROUTE:string = `${AUTH_ROUTES}/update-profile`
