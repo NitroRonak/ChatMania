4f75a1bfdccb8fcf50671914a43e792c47b9eb39
NitroRonak <ronaks0988@gmail.com>
2025-03-13 14:13:30 +0530

    profile update constant added

--- Client/src/utils/constants.ts
+++ Client/src/utils/constants.ts
@@ -5,3 +5,5 @@ export const SIGNIN_ROUTE: string = `${AUTH_ROUTES}/login`
 export const GET_USER_INFO:string = `${AUTH_ROUTES}/user-info`
 
 export const UPDATE_PROFILE_ROUTE:string = `${AUTH_ROUTES}/update-profile`
+
+export const UPDATE_PROFILE_IMAGE_ROUTE:string = `${AUTH_ROUTES}/update-profile-image`
