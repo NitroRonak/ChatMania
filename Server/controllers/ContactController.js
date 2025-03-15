b9bf1f5aaa10d9347cf9297db7e0521e1cc7a905
NitroRonak <ronaks0988@gmail.com>
2025-03-16 00:16:17 +0530

    get contacts for dm list contoller done.

--- Server/controllers/ContactController.js
+++ Server/controllers/ContactController.js
@@ -1,5 +1,5 @@
 import User from "../models/UserModel.js";
-
+import { Message } from "../models/MessagesModel.js";
 export const searchContact = async(req, res) => {
     const { searchTerm } = req.body;
     if(searchTerm === undefined || searchTerm === null) {
@@ -27,4 +27,70 @@ export const searchContact = async(req, res) => {
     return res.status(200).json({
         contacts
     });
+}
+
+export const getContactsForDmList = async(req, res) => {
+    try {
+       let userId=req.userId;
+       userId=new mongoose.Types.ObjectId(userId);
+       const contacts = await Message.aggregate([
+        {$match:{
+            $or:[
+                {sender:userId},
+                {recepient:userId}
+            ]
+        }},
+        {
+            $sort:{
+                timestamp:-1
+            }
+        },
+        {
+            $group:{
+                _id:{
+                    $cond:{
+                        if:{$eq:["$sender",userId]},
+                        then:"$recepient",
+                        else:"$sender"
+                    }
+                },
+                lastMessageTime:{
+                    $first:"$timestamp"
+                }
+            }
+        },
+        {
+            $lookup:{
+                from:"users",
+                localField:"_id",
+                foreignField:"_id",
+                as:"contactInfo"
+            }
+        },
+        {
+            $unwind:"$contactInfo"
+        },
+        {
+            $project:{
+                _id:1,
+                lastMessageTime:1,
+                email:"$contactInfo.email",
+                firstName:"$contactInfo.firstName",
+                lastName:"$contactInfo.lastName",
+                image:"$contactInfo.image",
+                color:"$contactInfo.color"
+            }
+        },
+        {
+            $sort:{
+                lastMessageTime:-1
+            }
+        }
+       ])
+       return res.status(200).json({
+        contacts
+       })
+    } catch (error) {
+        return res.status(500).send("Internal server error")
+    }            
 }
