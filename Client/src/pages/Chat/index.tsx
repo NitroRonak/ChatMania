537b5d32ef57266c9ed64bfcf84db57d18bc6dfb
NitroRonak <ronaks0988@gmail.com>
2025-03-16 03:33:37 +0530

    created state for uploading and downloading file for showing progress animation.

--- Client/src/pages/Chat/index.tsx
+++ Client/src/pages/Chat/index.tsx
@@ -7,7 +7,15 @@ import ChatContainer from "./components/Chat-Container";
 import EmptyChatContainer from "./components/Empty-Chat-Container";
 
 const Chat = () => {
-  const {userInfo,selectedChatType,selectedChatData} = useAppStore();
+  const {
+    userInfo,
+    selectedChatType,
+    selectedChatData,
+    isUploading,
+    isDownloading,
+    fileUploadProgress,
+    fileDownloadProgress,
+  } = useAppStore();
   const navigate= useNavigate();
   useEffect(()=>{
     if(!userInfo?.profileSetup){
@@ -17,6 +25,18 @@ const Chat = () => {
   },[userInfo,navigate])
   return (
     <div className="flex w-full h-[100vh] text-white overflow-hidden">
+      {
+        isUploading && <div className="fixed top-0 left-0 w-full h-full bg-black/50 flex items-center justify-center z-10 flex-col gap-5 backdrop-blur-lg">
+          <h5 className="text-5xl animate-pulse">Uploading File</h5>
+          {fileUploadProgress}%
+        </div>
+      }
+      {
+        isDownloading && <div className="fixed top-0 left-0 w-full h-full bg-black/50 flex items-center justify-center z-10 flex-col gap-5 backdrop-blur-lg">
+          <h5 className="text-5xl animate-pulse">Downloading File</h5>
+          {fileDownloadProgress}%
+        </div>
+      }
       <ContactsContainer/>
       {
         selectedChatType === undefined ?
