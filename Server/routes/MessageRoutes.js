e496cd9bb50231cbc7d3e619243178561464065d
NitroRonak <ronaks0988@gmail.com>
2025-03-16 02:01:32 +0530

    Message Route for file sent by user has done.

--- Server/routes/MessageRoutes.js
+++ Server/routes/MessageRoutes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { getMessages } from "../controllers/MessageController.js";
+import { getMessages,uploadFile } from "../controllers/MessageController.js";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
+import multer from "multer";
 
 const router = Router();
+const upload = multer({
+    dest: "uploads/files",
+});
 
 router.post("/get-messages", verifyToken,getMessages);
+router.post("/upload-file", verifyToken,upload.single("file"),uploadFile);
 
 export default router;
