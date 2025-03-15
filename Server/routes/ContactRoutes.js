59f38fc67f536a8f5cee6850fe3d6c09e92661b3
NitroRonak <ronaks0988@gmail.com>
2025-03-16 00:16:35 +0530

    get contacts for dm list route done.

--- Server/routes/ContactRoutes.js
+++ Server/routes/ContactRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { searchContact } from "../controllers/ContactController.js";
+import { getContactsForDmList, searchContact } from "../controllers/ContactController.js";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
 
 const router = express.Router();
 
 router.post("/search", verifyToken, searchContact);
+router.get("/get-contacts-for-dm", verifyToken, getContactsForDmList);
 
 export default router;
