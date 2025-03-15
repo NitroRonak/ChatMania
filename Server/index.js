7d1b75ea846f4fa5e7fb6d8a1f52ca6e336bfb80
NitroRonak <ronaks0988@gmail.com>
2025-03-15 22:45:11 +0530

    message route middleware setup done.

--- Server/index.js
+++ Server/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import AuthRoutes from "./routes/AuthRoutes.js";
 import ContactRoutes from "./routes/ContactRoutes.js";
+import MessageRoutes from "./routes/MessageRoutes.js";
 import setupSocket from "./socket.js";
 dotenv.config();
 const app = express();
@@ -20,7 +21,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth",AuthRoutes);
 app.use("/api/contacts",ContactRoutes);
-
+app.use("/api/messages",MessageRoutes);
 const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
