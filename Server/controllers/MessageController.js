2ac80913fa3c865755c2f00ab3badc6e29c2a80c
NitroRonak <ronaks0988@gmail.com>
2025-03-16 02:01:15 +0530

    Message Controller for file sent by user has done.

--- Server/controllers/MessageController.js
+++ Server/controllers/MessageController.js
@@ -1,30 +1,45 @@
 import User from "../models/UserModel.js";
 import { Message } from "../models/MessagesModel.js";
-export const getMessages = async(req, res) => {
-   try {
+import { mkdirSync, renameSync } from "fs";
+export const getMessages = async (req, res) => {
+  try {
     const user1 = req.userId;
     const user2 = req.body.id;
-    if(!user1 || !user2) {
-     return res.status(400).send("Both users id are required")
+    if (!user1 || !user2) {
+      return res.status(400).send("Both users id are required");
     }
     const messages = await Message.find({
-     $or: [
+      $or: [
         {
-         sender: user1,
-         recepient: user2
+          sender: user1,
+          recepient: user2,
         },
         {
-         sender: user2,
-         recepient: user1
-        }
-     ]
-    }).sort({timestamp: 1})
+          sender: user2,
+          recepient: user1,
+        },
+      ],
+    }).sort({ timestamp: 1 });
     return res.status(200).json({
-     messages
+      messages,
+    });
+  } catch (error) {
+    return res.status(500).send("Internal server error");
+  }
+};
+
+export const uploadFile = async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).send("File is required");
+    }
+    const date = Date.now();
+    const fileDir = `uploads/files/${date}`;
+    const fileName = `${fileDir}/${req.file.originalname}`;
+    mkdirSync(fileDir, { recursive: true });
+    renameSync(req.file.path, fileName);
+    return res.status(200).json({
+      filePath: fileName,
     })
-   } catch (error) {
-    return res.status(500).send("Internal server error")
-   }
-   
-   
-}
+  } catch (error) {}
+};
