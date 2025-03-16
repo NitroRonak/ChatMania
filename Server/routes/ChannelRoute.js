b27e2e313f4ab8b1bfaa4299ea1d9a9284ef407f
NitroRonak <ronaks0988@gmail.com>
2025-03-16 18:45:17 +0530

    get all channel route done

--- Server/routes/ChannelRoute.js
+++ Server/routes/ChannelRoute.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { createChannel } from "../controllers/ChannelController.js";
+import { createChannel, getUserChannels } from "../controllers/ChannelController.js";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
 const router = Router();
 
 router.post("/create-channel", verifyToken, createChannel);
-
+router.get("/get-user-channels", verifyToken, getUserChannels);
 export default router;
