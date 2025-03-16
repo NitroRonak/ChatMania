6a2009f3565dd1fb2ca8c88a1dd9c3b60856531c
NitroRonak <ronaks0988@gmail.com>
2025-03-17 00:32:55 +0530

    handle channel receive message funtionality done

--- Client/src/context/SocketContext.tsx
+++ Client/src/context/SocketContext.tsx
@@ -31,7 +31,14 @@ export const SocketProvider = ({children}:{children:React.ReactNode}) => {
                     addMessage(message);
                 }
             }
+            const handleReceiveChannelMessage = (message:any)=>{
+                const {selectedChatData,selectedChatType,addMessage}=useAppStore.getState();
+                if(selectedChatType !== undefined && selectedChatData._id===message.channelId){
+                    addMessage(message);
+                }
+            }
             socketRef.current.on("receiveMessage",handleReceiveMessage);
+            socketRef.current.on("receive-channel-message",handleReceiveChannelMessage);
             return ()=>{
                 socketRef.current.disconnect();
             }
