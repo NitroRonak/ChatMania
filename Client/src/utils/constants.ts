282b2fb7a3afa600e787b9d96d2af1a54d2230ba
NitroRonak <ronaks0988@gmail.com>
2025-03-16 18:09:30 +0530

    added channel route

--- Client/src/utils/constants.ts
+++ Client/src/utils/constants.ts
@@ -23,3 +23,6 @@ export const GET_CONTACTS_FOR_DM_LIST_ROUTE:string = `${CONTACT_ROUTES}/get-cont
 export const UPLOAD_FILE_ROUTE:string = `${MESSAGE_ROUTES}/upload-file`
 
 export const GET_ALL_CONTACTS_ROUTE:string = `${CONTACT_ROUTES}/get-all-contacts`
+
+export const CHANNEL_ROUTES:string = `/api/channel`
+export const CREATE_CHANNEL_ROUTE:string = `${CHANNEL_ROUTES}/create-channel`
