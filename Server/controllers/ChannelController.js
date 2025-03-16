0de16b56319d98c3e1da1ac47815a5939703154f
NitroRonak <ronaks0988@gmail.com>
2025-03-16 18:44:34 +0530

    get all channel controller done

--- Server/controllers/ChannelController.js
+++ Server/controllers/ChannelController.js
@@ -1,18 +1,20 @@
-import { Channel } from "../models/ChannelModel";
-import User from "../models/UserModel";
+import mongoose from "mongoose";
+import { Channel } from "../models/ChannelModel.js";
+import User from "../models/UserModel.js";
 export const createChannel = async (req, res) => {
     try {
         const { name, members } = req.body;
-        const admin = await User.findById(req.user._id);
+        
+        const admin = await User.findById(req.userId);
         if (!admin) {
             return res.status(404).send("Admin not found");
         }
 
         const validateMembers = await User.find({ _id: { $in: members } });
+        
         if (validateMembers.length !== members.length) {
             return res.status(400).send("Invalid members");
         }
-
         const newChannel = new Channel({ name, members, admin });
         await newChannel.save();
         res.status(201).json({
@@ -24,4 +26,20 @@ export const createChannel = async (req, res) => {
     }
 }
 
-
+export const getUserChannels = async (req, res) => {
+    try {
+        const userId = new mongoose.Types.ObjectId(req.userId);
+        const channels = await Channel.find({ 
+            $or: [
+                { admin: userId },
+                { members: userId }
+            ]
+         }).sort({ updatedAt: -1 });
+        res.status(200).json({
+            message: "Channels fetched successfully",
+            channels: channels
+        });
+    } catch (error) {
+        res.status(500).send("Internal Server Error");
+    }
+}
